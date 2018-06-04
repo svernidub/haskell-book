e3ace453e8be2f21bb3eca4162cd96079f24fdb6
Serhii Vernydub <svernidub@gmail.com>
2018-06-04 08:00:29 +0300

    Add State.

---
+++ monads/Kleisli.hs
@@ -0,0 +1,39 @@
+module Kleisli where
+
+import Prelude hiding(id, (>>), (*>))
+
+infixr 0 +$, *$
+
+
+class Category cat where
+    id   :: cat a a
+    (>>) :: cat a b -> cat b c -> cat a c
+
+     
+class Kleisli m where
+    idK  :: a -> m a
+    (*>) :: (a -> m b) -> (b -> m c) -> (a -> m c)
+
+
+instance Category (->) where
+    id x   = x
+    f >> g = g . f
+
+
+instance Kleisli Maybe where
+    idK = Just
+    f *> g = \a -> case f a of
+                        Nothing -> Nothing
+                        Just b  -> g b
+
+
+(+>) :: Kleisli m => (a -> m b) -> (b -> c) -> (a -> m c)
+f +> g = f *> (g >> idK)
+
+
+(*$) :: Kleisli m => (a -> m b) -> m a -> m b
+f *$ a = (const a *> f) ()
+
+
+(+$) :: Kleisli m => (a -> b) -> m a -> m b
+f +$ a = (const a +> f) ()
---
+++ monads/State.hs
@@ -0,0 +1,43 @@
+module Monads.State (
+    State(..),
+    runState
+) where
+
+
+import Kleisli(Kleisli(..))
+    
+
+data State s a = State (s -> (a, s))
+
+
+instance Functor (State s) where
+    fmap f s = State $ \s0 -> 
+                        let (a, s1) = runState s s0
+                        in (f a, s1)
+                              
+
+instance Applicative (State s) where
+    pure x = State $ \s0 -> (x, s0)
+    fstate <*> state = State $ \s0 ->
+                            let (f, s1) = runState fstate s0
+                                (a, st2) = runState state s1
+                            in (f a, st2)
+
+
+instance Monad (State s) where
+    return v     = State $ \s -> (v, s)
+    state >>= g  = State $ \s0 ->   let (v1, s1) = runState state s0
+                                        (v2, s2) = runState (g v1) s1
+                                    in (v2, s2)
+
+
+instance Kleisli (State s) where
+    idK a = State $ \s -> (a, s)
+    (*>) f g v = State $ \s0 ->
+                            let (v1, s1) = runState (f v) s0
+                                (v2, s2) = runState (g v1) s1
+                            in (v2, s2)
+    
+
+runState :: State s a -> s -> (a, s)
+runState (State f) = f
