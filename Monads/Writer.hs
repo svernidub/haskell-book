f5c481e066829d7def11fef28ee87af9093e9275
Serhii Vernydub <svernidub@gmail.com>
2018-06-05 08:08:26 +0300

    Make Writer message Monoid.

--- Monads/Writer.hs
+++ Monads/Writer.hs
@@ -15,21 +15,25 @@ instance Functor (Writer msg) where
     fmap f w = Writer (f a, w1) where (a, w1) = runWriter w
   
 
-instance Applicative (Writer msg) where
-    pure x  = error ""
+instance Monoid msg => Applicative (Writer msg) where
+    pure x  = Writer (x, mempty)
     fw <*> w = let (f, w1) = runWriter fw
                    (a, w2) = runWriter w
-               in Writer (f a, w2)
+               in Writer (f a, mappend w1 w2)
 
 
-instance Monad (Writer msg) where
-    return v = error ""
-    w >>= g = g a where (a, w1) = runWriter w
+instance Monoid msg => Monad (Writer msg) where
+    return v = Writer (v, mempty)
+    w >>= g = let (a1, w1) = runWriter w
+                  (a2, w2) = runWriter $ g a1
+              in Writer (a2, mappend w1 w2)
 
 
-instance Kleisli (Writer msg) where
-    idK x = error ""
-    (*>) f g v = g v1 where (v1, w1) = runWriter (f v)
+instance Monoid msg => Kleisli (Writer msg) where
+    idK x = Writer (x, mempty)
+    (*>) f g v = let (v1, w1) = runWriter $ f v
+                     (v2, w2) = runWriter $ g v1
+                 in Writer (v2, mappend w1 w2)
 
 
 runWriter :: Writer msg b -> (b, msg)
